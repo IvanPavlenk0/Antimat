828cb03550f7d10743d07c976c52033e76735935
Ivan Pavlenko <ivanpavlenko.025@gmail.com>
2021-07-27 20:18:41 +0300

    Initial Commit

---
+++ Antimat/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Antimat
+//
+//  Created by Ivan Pavlenko on 26.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        task10()
+    }
+    
+    
+    func task10() {
+
+        let badWordsText = "My pick is great, my pick is very great"
+        let filterWords = ["pick", "great"]
+         
+        print(antiMat(text: badWordsText, filter: filterWords))
+    }
+    
+    func antiMat(text: String, filter: Array<String>) -> String {
+        
+        var fixedText = text
+         
+        for mat in filter {
+
+            let lengthMat = mat.count
+            var position = 0
+           
+            while position != text.count {
+               
+                if text.count - position  >= lengthMat {
+
+                    let range = fixedText[fixedText.index(fixedText.startIndex, offsetBy: position)..<fixedText.index(fixedText.startIndex, offsetBy: position + lengthMat)]
+                    
+                    if range == mat {
+                        let cutRange = fixedText.index(fixedText.startIndex, offsetBy: position)..<fixedText.index(fixedText.startIndex, offsetBy: position + lengthMat)
+                        fixedText.removeSubrange(cutRange)
+                        fixedText.insert(contentsOf:starOnMat(mat: mat), at: fixedText.index(fixedText.startIndex, offsetBy: position))
+                        position += lengthMat
+                    } else {
+                       
+                        position += 1
+                    }
+                    
+                } else {
+                    position = text.count
+                }
+
+            }
+            
+        }
+        return fixedText
+    }
+
+    func starOnMat(mat: String) -> String {
+        var starRow = ""
+        for _ in 0..<mat.count {
+            starRow += "*"
+        }
+        return starRow
+    }
+
+
+}
+
